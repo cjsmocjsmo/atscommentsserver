321b4de6899f94af6c83ba7a9d55e978b32f72e4
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-02-04 05:32:02 -0800

    added service file and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
 sudo apt-get -y update && \
 sudo apt-get -y dist-upgrade && \
 sudo apt-get -y golang && \
 sudo apt-get -y autoclean && \
 sudo apt-get -y autoremove && \
-mkdir ./log/logfile.txt && \ 
+mkdir ./log && \ 
 mkdir ./static && / #for pics
 mkdir ./data && /
 mkdir ./data/accepted && \ 
+mkdir ./data/admin && \
+mkdir ./data/admin/profiles && \ 
+mkdir ./data/backups && \ 
+mkdir ./data/estcompleted && \ 
+mkdir ./data/estimates && \ 
+mkdir ./data/jailed && \ 
 mkdir ./data/rejected && \
-mkdir ./data/jailed
 
+touch ./log/logfile.txt && \ 
+touch ./data/admin/loggedinList.json && \ 
 
-go get -v /go/src/atsflutter
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main /go/src/atsflutter
+sudo mv ./ATSCommentsServer.service /etc/systemd/system/
+
+go get -v /home/porthose_cjsmo_cjsmo/atscommentsserver/
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o atscommentsserver /home/porthose_cjsmo_cjsmo/atscommentsserver/
+
+sudo systemctl start ATSCommentsServer
