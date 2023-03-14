089b116fe59c2038552ee2cadecfc98afa2cc45f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-03-13 20:28:03 -0700

    update

--- setup.sh
+++ setup.sh
@@ -2,28 +2,28 @@
 
 sudo apt-get -y update && \
 sudo apt-get -y dist-upgrade && \
-sudo apt-get -y golang && \
+sudo apt-get -y install golang git && \
 sudo apt-get -y autoclean && \
-sudo apt-get -y autoremove && \
-mkdir ./log && \ 
-mkdir ./static && / #for pics
-mkdir ./data && /
-mkdir ./data/accepted && \ 
-mkdir ./data/admin && \
-mkdir ./data/admin/profiles && \ 
-mkdir ./data/backups && \ 
-mkdir ./data/estcompleted && \ 
-mkdir ./data/estimates && \ 
-mkdir ./data/jailed && \ 
-mkdir ./data/rejected && \
+sudo apt-get -y autoremove
+# mkdir ./log && \ 
+# mkdir ./static && / #for pics
+# mkdir ./data && /
+# mkdir ./data/accepted && \ 
+# mkdir ./data/admin && \
+# mkdir ./data/admin/profiles && \ 
+# mkdir ./data/backups && \ 
+# mkdir ./data/estcompleted && \ 
+# mkdir ./data/estimates && \ 
+# mkdir ./data/jailed && \ 
+# mkdir ./data/rejected && \
 
-touch ./log/logfile.txt && \ 
-touch ./data/admin/loggedinList.json && \ 
+# touch ./log/logfile.txt && \ 
+# touch ./data/admin/loggedinList.json && \ 
 
-sudo mv ./ATSCommentsServer.service /etc/systemd/system/
+# sudo mv ./ATSCommentsServer.service /etc/systemd/system/
 
-go get -v /home/porthose_cjsmo_cjsmo/atscommentsserver/atscommentsserver/
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o atscommentsserver /home/porthose_cjsmo_cjsmo/atscommentsserver/atscommentsserver/
+# go get -v /home/porthose_cjsmo_cjsmo/atscommentsserver/atscommentsserver/
+# CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o atscommentsserver /home/porthose_cjsmo_cjsmo/atscommentsserver/atscommentsserver/
 
-sudo systemctl start ATSCommentsServer
+# sudo systemctl start ATSCommentsServer
 
